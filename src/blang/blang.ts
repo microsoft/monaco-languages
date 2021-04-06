58f8725e69e54cad83f107fc367297fc0a810fcc
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-04-06 13:31:40 -0400

    Update blang.ts

--- src/blang/blang.ts
+++ src/blang/blang.ts
@@ -22,7 +22,8 @@ export const language = <languages.IMonarchLanguage>{
 
 	// we include these common regular expressions
 	escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-
+	
+	keywords: /say|add|ta|ram|inp|is|lop|div|mpy/,
 	// The main tokenizer for our languages
 	tokenizer: {
 		root: [
