3b160a4349dd9be2a44998a67aa8173f69d54546
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-04-06 13:14:10 -0400

    Update blang.ts

--- src/blang/blang.ts
+++ src/blang/blang.ts
@@ -1,4 +1,4 @@
- *  Copyright (c) Microsoft Corporation. All rights reserved.
+ /*  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -6,32 +6,19 @@ import type { languages } from '../fillers/monaco-editor-core';
 
 export const conf: languages.LanguageConfiguration = {
 	comments: {
-		lineComment: '#'
+		lineComment: ' '
 	},
 	brackets: [
-		['{', '}'],
-		['[', ']'],
-		['(', ')']
 	],
 	autoClosingPairs: [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '"', close: '"' },
-		{ open: "'", close: "'" }
 	],
 	surroundingPairs: [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '"', close: '"' },
-		{ open: "'", close: "'" }
 	]
 };
 
 export const language = <languages.IMonarchLanguage>{
 	defaultToken: '',
-	tokenPostfix: '.ini',
+	tokenPostfix: '.blang',
 
 	// we include these common regular expressions
 	escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
@@ -39,43 +26,16 @@ export const language = <languages.IMonarchLanguage>{
 	// The main tokenizer for our languages
 	tokenizer: {
 		root: [
-			// sections
-			[/^\[[^\]]*\]/, 'metatag'],
-
-			// keys
-			[/(^\w+)(\s*)(\=)/, ['key', '', 'delimiter']],
-
 			// whitespace
 			{ include: '@whitespace' },
 
 			// numbers
 			[/\d+/, 'number'],
-
-			// strings: recover on non-terminated strings
-			[/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
-			[/'([^'\\]|\\.)*$/, 'string.invalid'], // non-teminated string
-			[/"/, 'string', '@string."'],
-			[/'/, 'string', "@string.'"]
 		],
 
 		whitespace: [
 			[/[ \t\r\n]+/, ''],
 			[/^\s*[#;].*$/, 'comment']
 		],
-
-		string: [
-			[/[^\\"']+/, 'string'],
-			[/@escapes/, 'string.escape'],
-			[/\\./, 'string.escape.invalid'],
-			[
-				/["']/,
-				{
-					cases: {
-						'$#==$S2': { token: 'string', next: '@pop' },
-						'@default': 'string'
-					}
-				}
-			]
-		]
 	}
 };
