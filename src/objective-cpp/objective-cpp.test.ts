15872b37fd51919570b27c2585d36b0606b6ea62
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2021-10-13 16:00:24 +0200

    Postfix fix in tests

--- src/objective-cpp/objective-cpp.test.ts
+++ src/objective-cpp/objective-cpp.test.ts
@@ -5,38 +5,38 @@
 
 import { testTokenization } from '../test/testRunner';
 
-testTokenization('objective-c', [
+testTokenization('objective-cpp', [
 	// Keywords
 	[
 		{
 			line: '-(id) initWithParams:(id<anObject>) aHandler withDeviceStateManager:(id<anotherObject>) deviceStateManager',
 			tokens: [
 				{ startIndex: 0, type: '' },
-				{ startIndex: 1, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 2, type: 'keyword.objective-c' },
-				{ startIndex: 4, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' },
-				{ startIndex: 6, type: 'identifier.objective-c' },
-				{ startIndex: 20, type: 'delimiter.objective-c' },
-				{ startIndex: 21, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 22, type: 'keyword.objective-c' },
-				{ startIndex: 24, type: 'delimiter.angle.objective-c' },
-				{ startIndex: 25, type: 'identifier.objective-c' },
-				{ startIndex: 33, type: 'delimiter.angle.objective-c' },
-				{ startIndex: 34, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 35, type: 'white.objective-c' },
-				{ startIndex: 36, type: 'identifier.objective-c' },
-				{ startIndex: 44, type: 'white.objective-c' },
-				{ startIndex: 45, type: 'identifier.objective-c' },
-				{ startIndex: 67, type: 'delimiter.objective-c' },
-				{ startIndex: 68, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 69, type: 'keyword.objective-c' },
-				{ startIndex: 71, type: 'delimiter.angle.objective-c' },
-				{ startIndex: 72, type: 'identifier.objective-c' },
-				{ startIndex: 85, type: 'delimiter.angle.objective-c' },
-				{ startIndex: 86, type: 'delimiter.parenthesis.objective-c' },
-				{ startIndex: 87, type: 'white.objective-c' },
-				{ startIndex: 88, type: 'identifier.objective-c' }
+				{ startIndex: 1, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 2, type: 'keyword.objective-cpp' },
+				{ startIndex: 4, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' },
+				{ startIndex: 6, type: 'identifier.objective-cpp' },
+				{ startIndex: 20, type: 'delimiter.objective-cpp' },
+				{ startIndex: 21, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 22, type: 'keyword.objective-cpp' },
+				{ startIndex: 24, type: 'delimiter.angle.objective-cpp' },
+				{ startIndex: 25, type: 'identifier.objective-cpp' },
+				{ startIndex: 33, type: 'delimiter.angle.objective-cpp' },
+				{ startIndex: 34, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 35, type: 'white.objective-cpp' },
+				{ startIndex: 36, type: 'identifier.objective-cpp' },
+				{ startIndex: 44, type: 'white.objective-cpp' },
+				{ startIndex: 45, type: 'identifier.objective-cpp' },
+				{ startIndex: 67, type: 'delimiter.objective-cpp' },
+				{ startIndex: 68, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 69, type: 'keyword.objective-cpp' },
+				{ startIndex: 71, type: 'delimiter.angle.objective-cpp' },
+				{ startIndex: 72, type: 'identifier.objective-cpp' },
+				{ startIndex: 85, type: 'delimiter.angle.objective-cpp' },
+				{ startIndex: 86, type: 'delimiter.parenthesis.objective-cpp' },
+				{ startIndex: 87, type: 'white.objective-cpp' },
+				{ startIndex: 88, type: 'identifier.objective-cpp' }
 			]
 		}
 	],
@@ -45,7 +45,7 @@ testTokenization('objective-c', [
 	[
 		{
 			line: '//',
-			tokens: [{ startIndex: 0, type: 'comment.objective-c' }]
+			tokens: [{ startIndex: 0, type: 'comment.objective-cpp' }]
 		}
 	],
 
@@ -53,8 +53,8 @@ testTokenization('objective-c', [
 		{
 			line: '    // a comment',
 			tokens: [
-				{ startIndex: 0, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'comment.objective-c' }
+				{ startIndex: 0, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'comment.objective-cpp' }
 			]
 		}
 	],
@@ -62,14 +62,14 @@ testTokenization('objective-c', [
 	[
 		{
 			line: '// a comment',
-			tokens: [{ startIndex: 0, type: 'comment.objective-c' }]
+			tokens: [{ startIndex: 0, type: 'comment.objective-cpp' }]
 		}
 	],
 
 	[
 		{
 			line: '//sticky comment',
-			tokens: [{ startIndex: 0, type: 'comment.objective-c' }]
+			tokens: [{ startIndex: 0, type: 'comment.objective-cpp' }]
 		}
 	],
 
@@ -77,12 +77,12 @@ testTokenization('objective-c', [
 		{
 			line: '/almost a comment',
 			tokens: [
-				{ startIndex: 0, type: 'operator.objective-c' },
-				{ startIndex: 1, type: 'identifier.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' },
-				{ startIndex: 8, type: 'identifier.objective-c' },
-				{ startIndex: 9, type: 'white.objective-c' },
-				{ startIndex: 10, type: 'identifier.objective-c' }
+				{ startIndex: 0, type: 'operator.objective-cpp' },
+				{ startIndex: 1, type: 'identifier.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' },
+				{ startIndex: 8, type: 'identifier.objective-cpp' },
+				{ startIndex: 9, type: 'white.objective-cpp' },
+				{ startIndex: 10, type: 'identifier.objective-cpp' }
 			]
 		}
 	],
@@ -91,14 +91,14 @@ testTokenization('objective-c', [
 		{
 			line: '1 / 2; /* comment',
 			tokens: [
-				{ startIndex: 0, type: 'number.objective-c' },
-				{ startIndex: 1, type: 'white.objective-c' },
-				{ startIndex: 2, type: 'operator.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'number.objective-c' },
-				{ startIndex: 5, type: 'delimiter.objective-c' },
-				{ startIndex: 6, type: 'white.objective-c' },
-				{ startIndex: 7, type: 'comment.objective-c' }
+				{ startIndex: 0, type: 'number.objective-cpp' },
+				{ startIndex: 1, type: 'white.objective-cpp' },
+				{ startIndex: 2, type: 'operator.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'number.objective-cpp' },
+				{ startIndex: 5, type: 'delimiter.objective-cpp' },
+				{ startIndex: 6, type: 'white.objective-cpp' },
+				{ startIndex: 7, type: 'comment.objective-cpp' }
 			]
 		}
 	],
@@ -107,16 +107,16 @@ testTokenization('objective-c', [
 		{
 			line: 'int x = 1; // my comment // is a nice one',
 			tokens: [
-				{ startIndex: 0, type: 'keyword.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'identifier.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' },
-				{ startIndex: 6, type: 'operator.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' },
-				{ startIndex: 8, type: 'number.objective-c' },
-				{ startIndex: 9, type: 'delimiter.objective-c' },
-				{ startIndex: 10, type: 'white.objective-c' },
-				{ startIndex: 11, type: 'comment.objective-c' }
+				{ startIndex: 0, type: 'keyword.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'identifier.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' },
+				{ startIndex: 6, type: 'operator.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' },
+				{ startIndex: 8, type: 'number.objective-cpp' },
+				{ startIndex: 9, type: 'delimiter.objective-cpp' },
+				{ startIndex: 10, type: 'white.objective-cpp' },
+				{ startIndex: 11, type: 'comment.objective-cpp' }
 			]
 		}
 	],
@@ -125,7 +125,7 @@ testTokenization('objective-c', [
 	[
 		{
 			line: '/* a simple comment */',
-			tokens: [{ startIndex: 0, type: 'comment.objective-c' }]
+			tokens: [{ startIndex: 0, type: 'comment.objective-cpp' }]
 		}
 	],
 
@@ -133,16 +133,16 @@ testTokenization('objective-c', [
 		{
 			line: 'int x = /* embedded comment */ 1;',
 			tokens: [
-				{ startIndex: 0, type: 'keyword.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'identifier.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' },
-				{ startIndex: 6, type: 'operator.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' },
-				{ startIndex: 8, type: 'comment.objective-c' },
-				{ startIndex: 30, type: 'white.objective-c' },
-				{ startIndex: 31, type: 'number.objective-c' },
-				{ startIndex: 32, type: 'delimiter.objective-c' }
+				{ startIndex: 0, type: 'keyword.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'identifier.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' },
+				{ startIndex: 6, type: 'operator.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' },
+				{ startIndex: 8, type: 'comment.objective-cpp' },
+				{ startIndex: 30, type: 'white.objective-cpp' },
+				{ startIndex: 31, type: 'number.objective-cpp' },
+				{ startIndex: 32, type: 'delimiter.objective-cpp' }
 			]
 		}
 	],
@@ -151,18 +151,18 @@ testTokenization('objective-c', [
 		{
 			line: 'int x = /* comment and syntax error*/ 1; */',
 			tokens: [
-				{ startIndex: 0, type: 'keyword.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'identifier.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' },
-				{ startIndex: 6, type: 'operator.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' },
-				{ startIndex: 8, type: 'comment.objective-c' },
-				{ startIndex: 37, type: 'white.objective-c' },
-				{ startIndex: 38, type: 'number.objective-c' },
-				{ startIndex: 39, type: 'delimiter.objective-c' },
-				{ startIndex: 40, type: 'white.objective-c' },
-				{ startIndex: 41, type: 'operator.objective-c' }
+				{ startIndex: 0, type: 'keyword.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'identifier.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' },
+				{ startIndex: 6, type: 'operator.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' },
+				{ startIndex: 8, type: 'comment.objective-cpp' },
+				{ startIndex: 37, type: 'white.objective-cpp' },
+				{ startIndex: 38, type: 'number.objective-cpp' },
+				{ startIndex: 39, type: 'delimiter.objective-cpp' },
+				{ startIndex: 40, type: 'white.objective-cpp' },
+				{ startIndex: 41, type: 'operator.objective-cpp' }
 			]
 		}
 	],
@@ -171,12 +171,12 @@ testTokenization('objective-c', [
 		{
 			line: 'x = /**/;',
 			tokens: [
-				{ startIndex: 0, type: 'identifier.objective-c' },
-				{ startIndex: 1, type: 'white.objective-c' },
-				{ startIndex: 2, type: 'operator.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'comment.objective-c' },
-				{ startIndex: 8, type: 'delimiter.objective-c' }
+				{ startIndex: 0, type: 'identifier.objective-cpp' },
+				{ startIndex: 1, type: 'white.objective-cpp' },
+				{ startIndex: 2, type: 'operator.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'comment.objective-cpp' },
+				{ startIndex: 8, type: 'delimiter.objective-cpp' }
 			]
 		}
 	],
@@ -185,11 +185,11 @@ testTokenization('objective-c', [
 		{
 			line: 'x = /*/;',
 			tokens: [
-				{ startIndex: 0, type: 'identifier.objective-c' },
-				{ startIndex: 1, type: 'white.objective-c' },
-				{ startIndex: 2, type: 'operator.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'comment.objective-c' }
+				{ startIndex: 0, type: 'identifier.objective-cpp' },
+				{ startIndex: 1, type: 'white.objective-cpp' },
+				{ startIndex: 2, type: 'operator.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'comment.objective-cpp' }
 			]
 		}
 	],
@@ -199,13 +199,13 @@ testTokenization('objective-c', [
 		{
 			line: '#import <GTLT.h>',
 			tokens: [
-				{ startIndex: 0, type: 'keyword.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' },
-				{ startIndex: 8, type: 'delimiter.angle.objective-c' },
-				{ startIndex: 9, type: 'identifier.objective-c' },
+				{ startIndex: 0, type: 'keyword.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' },
+				{ startIndex: 8, type: 'delimiter.angle.objective-cpp' },
+				{ startIndex: 9, type: 'identifier.objective-cpp' },
 				{ startIndex: 13, type: '' },
-				{ startIndex: 14, type: 'identifier.objective-c' },
-				{ startIndex: 15, type: 'delimiter.angle.objective-c' }
+				{ startIndex: 14, type: 'identifier.objective-cpp' },
+				{ startIndex: 15, type: 'delimiter.angle.objective-cpp' }
 			]
 		}
 	],
@@ -215,8 +215,8 @@ testTokenization('objective-c', [
 		{
 			line: '0 ',
 			tokens: [
-				{ startIndex: 0, type: 'number.objective-c' },
-				{ startIndex: 1, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.objective-cpp' },
+				{ startIndex: 1, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -225,8 +225,8 @@ testTokenization('objective-c', [
 		{
 			line: '0x ',
 			tokens: [
-				{ startIndex: 0, type: 'number.hex.objective-c' },
-				{ startIndex: 2, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.hex.objective-cpp' },
+				{ startIndex: 2, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -235,8 +235,8 @@ testTokenization('objective-c', [
 		{
 			line: '0x123 ',
 			tokens: [
-				{ startIndex: 0, type: 'number.hex.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.hex.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -245,8 +245,8 @@ testTokenization('objective-c', [
 		{
 			line: '23.5 ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 4, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 4, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -255,8 +255,8 @@ testTokenization('objective-c', [
 		{
 			line: '23.5e3 ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 6, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 6, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -265,8 +265,8 @@ testTokenization('objective-c', [
 		{
 			line: '23.5E3 ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 6, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 6, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -275,8 +275,8 @@ testTokenization('objective-c', [
 		{
 			line: '23.5F ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -285,8 +285,8 @@ testTokenization('objective-c', [
 		{
 			line: '23.5f ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 5, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 5, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -295,8 +295,8 @@ testTokenization('objective-c', [
 		{
 			line: '1.72E3F ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -305,8 +305,8 @@ testTokenization('objective-c', [
 		{
 			line: '1.72E3f ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -315,8 +315,8 @@ testTokenization('objective-c', [
 		{
 			line: '1.72e3F ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -325,8 +325,8 @@ testTokenization('objective-c', [
 		{
 			line: '1.72e3f ',
 			tokens: [
-				{ startIndex: 0, type: 'number.float.objective-c' },
-				{ startIndex: 7, type: 'white.objective-c' }
+				{ startIndex: 0, type: 'number.float.objective-cpp' },
+				{ startIndex: 7, type: 'white.objective-cpp' }
 			]
 		}
 	],
@@ -335,9 +335,9 @@ testTokenization('objective-c', [
 		{
 			line: '0+0',
 			tokens: [
-				{ startIndex: 0, type: 'number.objective-c' },
-				{ startIndex: 1, type: 'operator.objective-c' },
-				{ startIndex: 2, type: 'number.objective-c' }
+				{ startIndex: 0, type: 'number.objective-cpp' },
+				{ startIndex: 1, type: 'operator.objective-cpp' },
+				{ startIndex: 2, type: 'number.objective-cpp' }
 			]
 		}
 	],
@@ -346,9 +346,9 @@ testTokenization('objective-c', [
 		{
 			line: '100+10',
 			tokens: [
-				{ startIndex: 0, type: 'number.objective-c' },
-				{ startIndex: 3, type: 'operator.objective-c' },
-				{ startIndex: 4, type: 'number.objective-c' }
+				{ startIndex: 0, type: 'number.objective-cpp' },
+				{ startIndex: 3, type: 'operator.objective-cpp' },
+				{ startIndex: 4, type: 'number.objective-cpp' }
 			]
 		}
 	],
@@ -357,11 +357,11 @@ testTokenization('objective-c', [
 		{
 			line: '0 + 0',
 			tokens: [
-				{ startIndex: 0, type: 'number.objective-c' },
-				{ startIndex: 1, type: 'white.objective-c' },
-				{ startIndex: 2, type: 'operator.objective-c' },
-				{ startIndex: 3, type: 'white.objective-c' },
-				{ startIndex: 4, type: 'number.objective-c' }
+				{ startIndex: 0, type: 'number.objective-cpp' },
+				{ startIndex: 1, type: 'white.objective-cpp' },
+				{ startIndex: 2, type: 'operator.objective-cpp' },
+				{ startIndex: 3, type: 'white.objective-cpp' },
+				{ startIndex: 4, type: 'number.objective-cpp' }
 			]
 		}
 	]
